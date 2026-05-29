77003e10ea3dc975d85d7a58955a1c12d385939c
Juan Montero <juan_mm_jose@hotmail.com>
2026-05-29 03:06:19 +0200

    fix(storage): add StorageModule to AppModule as global provider root

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { join } from "node:path";
+import { StorageModule } from "./shared/providers/storage/storage.module";
 import { AchievementsModule } from "./modules/achievements/achievements.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { CommunityModule } from "./modules/community/community.module";
@@ -20,6 +21,7 @@ import { TopicsModule } from "./modules/topics/topics.module";
       isGlobal: true,
       envFilePath: join(process.cwd(), ".env")
     }),
+    StorageModule,
     AuthModule,
     AchievementsModule,
     CommunityModule,
--- apps/api/src/modules/files/files.module.ts
+++ apps/api/src/modules/files/files.module.ts
@@ -1,7 +1,5 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../../shared/database/database.module";
-import { StorageModule } from "../../shared/providers/storage/storage.module";
-import { AuthService } from "../auth/services/auth.service";
 import { AuthModule } from "../auth/auth.module";
 import { FilesController } from "./controllers/files.controller";
 import { PrismaFilesRepository } from "./repositories/prisma-files.repository";
@@ -9,7 +7,7 @@ import { FILES_REPOSITORY } from "./services/files.service.constants";
 import { FilesService } from "./services/files.service";
 
 @Module({
-  imports: [DatabaseModule, StorageModule, AuthModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [FilesController],
   providers: [
     FilesService,
